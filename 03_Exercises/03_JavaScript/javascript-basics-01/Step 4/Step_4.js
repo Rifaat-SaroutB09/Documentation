5d750f1adc8db4b2fc97df54d71b9020f76f478d
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2022-12-07 11:14:30 +0200

    Completed_step_4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step 4/Step_4.js
@@ -0,0 +1,6 @@
+let name1=prompt("what is your name ? ");
+let surname=prompt("what is your surname? ");
+let city=prompt("where do you live? ");
+
+alert("Hello " + name1 + " " + surname + " from " + city + " !");
+
