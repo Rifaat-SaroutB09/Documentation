da59a75d0cd58be4cc33ca49bfb36fbe3462e484
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2022-12-07 13:33:40 +0200

    Completed_Step_7

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step7/Step_7.js
@@ -0,0 +1,13 @@
+var message1=prompt("What is your shoes size? ");
+var message2=prompt("What is your birth year ?");
+
+function multipleOperations(message1,message2){
+ var x = message1 * 2;
+ var y = x + 5;
+ var a = y * 50;
+ var b = a - message2;
+ var result= b + 1766;
+
+ return window.alert(result) ; }
+
+multipleOperations(message1,message2);
