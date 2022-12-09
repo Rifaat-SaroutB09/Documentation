e4776fed42c23197b1fc1a6c72a944ad8ff54d3c
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2022-12-09 15:16:47 +0200

    completed-step-2

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,7 @@
+
+let element = document.querySelector("#image1");
+
+// onmouseover()//
+element.addEventListener('mouseover', function(){ 
+    element.src="./images/image1_2.jpg"
+})
---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 2/Step_2.js
@@ -0,0 +1,4 @@
+var input1=document.querySelector('input');
+input1.onblur=function(){
+    alert("Thank you for participating!")
+}
