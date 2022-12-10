54420162a47a30dd9ce1041956c5fe4d98d8883d
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2022-12-10 08:31:25 +0200

    completed-step-3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,10 @@
+
+function changeColorToGreen(){
+        document.querySelector('#text').style.color = "green";
+    }
+function changeColorToRed(){
+        document.querySelector('#text').style.color = "red";
+    }
+function changeColorToBlue(){
+        document.querySelector('#text').style.color = "blue";
+    }
