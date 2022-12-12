7ffc03e2d9e61f7c8f17dc10f1f7897285e9228a
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2022-12-12 23:26:11 +0200

    Updqted-Basic-Queries

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -9,7 +9,7 @@ SELECT Name from students WHERE (Age >30) ;
 4)SELECT Points FROM students Where (Name == 'Alex');
 
 5)
-INSERT INTO students VALUES (4,'Rifaat ALSAROUT' , 27 , 'M',27,250); 
+INSERT INTO students VALUES (7,'Rifaat ALSAROUT' , 27 , 'M',250); 
 
 6)
 UPDATE   students SET Points = 400 WHERE Name = 'Basma';
