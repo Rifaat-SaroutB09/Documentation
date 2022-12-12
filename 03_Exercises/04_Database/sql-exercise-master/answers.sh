3ec8f2028a86e0c9ea7e5043f72ffd6dc0b04c12
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2022-12-12 23:52:28 +0200

    joins

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -17,7 +17,7 @@ UPDATE   students SET Points = 400 WHERE Name = 'Basma';
 7)
 UPDATE   students SET Points = 100 WHERE Name = 'Alex';
 
-8)CREATE TABLE "graduates"(
+10)CREATE TABLE "graduates"(
        "ID"  INTEGER Not NULL ,
 	   "Name" TEXT NOT NULL UNIQUE ,
 	   "Age" TEXT ,
@@ -27,7 +27,26 @@ UPDATE   students SET Points = 100 WHERE Name = 'Alex';
 	   PRIMARY key ("ID" AUTOINCREMENT)
 	   );
 SELECT * from students ;
+	
+11)
+INSERT INTO graduates VALUES (1,'Layal' , 18 , 'M',300, 08/09/2018) ; 
+	
+12)
+DELETE  FROM students WHERE (name = 'layla');
 
-9)INSERT INTO graduates VALUES (1,'Layal' , 18 , 'M',300, 08/09/2018) ; 
+14)
+SELECT emloyees.name , companies.name as company , companies.date ,
+FROM companies,emloyees ,
+Where companies.name=emloyees.company ;
 
-1
+15)
+SELECT emloyees.name 
+FROM companies.emloyees 
+WHERE companies.name = emloyees.company AND companies.date <2000
+
+ 
+16)
+SELECT company 
+FROM emloyees
+WHERE Role = 'Graphic Designer '
+
