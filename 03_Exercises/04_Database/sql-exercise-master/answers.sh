3c6c97aa2820826e9f8cd9307dd176d90dfc3dd5
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2022-12-12 23:20:20 +0200

    Basic-Queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,20 @@
+1)
+SELECT * from students ;
+ 
+2)SELECT Name from students ;
+SELECT Name from students WHERE (Age >30) ;
+
+3)SELECT name from students Where (Age == 30) AND  (Gender == 'F');
+
+4)SELECT Points FROM students Where (Name == 'Alex');
+
+5)
+INSERT INTO students VALUES (4,'Rifaat ALSAROUT' , 27 , 'M',27,250); 
+
+6)
+UPDATE   students SET Points = 400 WHERE Name = 'Basma';
+
+7)
+UPDATE   students SET Points = 100 WHERE Name = 'Alex';
+
+
