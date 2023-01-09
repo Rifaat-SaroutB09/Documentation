a104d6f7136732382898310436dbf06af2f9cf23
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2023-01-09 07:25:37 +0200

    step4

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -25,6 +25,20 @@ let minutes = date_ob.getMinutes();
 // let seconds = date_ob.getSeconds();
 res.send({status:200, message:hours + ":" + minutes  })
 })
+//STEP 4: CREATE A ROUTE FOR HELLO, ID
+app.get('/hello/:id', (req,res) =>{
+  res.send({status:200, message:"Hello " + req.params.id});
+})
+
+//STEP 4: CREATE A ROUTE FOR SEARCH
+app.get('/search', (req, res) => {
+  let search = req.query.q;
+  console.log(search)
+  if(search == ""){
+      res.send({status:500, error:true, message:"you have to provide a search"});
+  }
+  res.send({status:200, message:"ok", data:search});
+})
 
 //START SERVER
 //app.listen(3000);
